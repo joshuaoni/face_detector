d0b8607417d1ea6144e5a1e43ed200c0d39517be
joshua oni <davidjoshua603@gmail.com>
2024-12-26 04:18:40 +0100

    debug

--- src/components/VideoRecorder.tsx
+++ src/components/VideoRecorder.tsx
@@ -126,7 +126,7 @@ const VideoRecorder: React.FC = () => {
       {videoUrl && (
         <div className="mt-6 w-full">
           <h3 className="text-lg font-medium mb-2">Recorded Video:</h3>
-          <video src={videoUrl} controls className="w-full rounded-lg shadow" autoPlay muted loop playsInline/>
+          <video src={videoUrl} controls preload="auto" playsInline className="w-full rounded-lg shadow" />
         </div>
       )}
       {videoUrl && (
